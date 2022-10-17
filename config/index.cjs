e85f0ed605396c82f2c4a76be4ea9cdf739b0f5a
杨子江 <yangzj@feiyu.com>
2022-10-17 18:16:01 +0800

    再一次模板测试

--- config/index.cjs
+++ config/index.cjs
@@ -66,7 +66,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oum2_6nkFAJpXnSblqsXkL0HDd10',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'h4siOKSySLhtkHScDWexvXBYk_OUwGZRUT-a1Sv6_Kc',
+      useTemplateId: 'bZLx7UoYk5gKjZzXmldXHhW8UzWuQ6OHtn2lZ6aYIlQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-21',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
