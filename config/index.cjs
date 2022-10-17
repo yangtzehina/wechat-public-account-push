2e6406da002a440fbabb870a4a2edb8fab05a533
杨子江 <yangzj@feiyu.com>
2022-10-17 17:20:07 +0800

    提交天行API的接口
    增加天行API接口

--- config/index.cjs
+++ config/index.cjs
@@ -16,6 +16,27 @@ const USER_CONFIG = {
   PROVINCE: '福建',
   CITY: '厦门',
 
+  TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: 'b47e5c73d1b78bc3b9bf3ee2eb48ea82',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: true,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: false,
+    
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 3,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 3,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'default',
+  },
+
   USERS: [
     {
       // 想要发送的人的名字
